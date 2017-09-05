aad1f9778c0d41d64405bf2b9143d8140d8e3b7c
Lavee Singh <archives987@gmail.com>
2017-09-06 02:26:09 +0530

    Added post_update view and added response codes to post_create view

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ import datetime
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from api.models import Post
 from api.serializers import PostSerializer
@@ -28,19 +29,30 @@ def post_list(request):
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
+
 @api_view(http_method_names=['POST'])
 def post_create(request):
     serializer = PostSerializer(data=request.POST)
     if serializer.is_valid():
-        MSG = "successfully created the post"
         post = serializer.save()
         post.creation = datetime.date.today
         post.last_edit = datetime.date.today
         post.save()
-    else:
-        MSG = "There's something wrong with submitted data"
-    response = {
-        'message': MSG,
-    }
-    return Response(response)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(http_method_names=['POST'])
+def post_update(request):
+    post_id = request.POST.get("id")
+    if not post_id:
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+    instance = Post.objects.get(id=post_id)
+    serializer = PostSerializer(instance, data=request.POST)
+    if serializer.is_valid():
+        post = serializer.save()
+        post.last_edit = datetime.date.today
+        post.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
